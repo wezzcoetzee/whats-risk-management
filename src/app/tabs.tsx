bc44f6499e854ea0fbba40790d1f003c756dd4ac
Wesley Coetzee <coetzeewr@gmail.com>
2024-09-20 09:21:45 +0200

    feat: add query params

--- src/app/tabs.tsx
+++ src/app/tabs.tsx
@@ -3,10 +3,18 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PositionCalculator from "./_calculators/position-calculator";
 import ProfitCalculator from "./_calculators/profit-calculator";
+import { useSearchParams } from "next/navigation";
 
 export default function TabsContainer() {
+  const searchParams = useSearchParams();
+  const param = searchParams.get("type");
+  const defaultValue = param === "position-size" ? "position-size" : "profit";
+
   return (
-    <Tabs defaultValue="profit" className="w-[360px] sm:w-[400px] md:w-[800px]">
+    <Tabs
+      defaultValue={defaultValue}
+      className="w-[360px] sm:w-[400px] md:w-[760px]"
+    >
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="profit">profit</TabsTrigger>
         <TabsTrigger value="position-size">position size</TabsTrigger>
