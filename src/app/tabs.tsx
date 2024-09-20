19f4dacb885c5bb14f2786ee210298c5ce1a9c18
Wesley Coetzee <coetzeewr@gmail.com>
2024-09-20 10:24:53 +0200

    feat: add icon and google analytics

--- src/app/tabs.tsx
+++ src/app/tabs.tsx
@@ -3,16 +3,27 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PositionCalculator from "./_calculators/position-calculator";
 import ProfitCalculator from "./_calculators/profit-calculator";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
 
 export default function TabsContainer() {
   const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
   const param = searchParams.get("type");
   const defaultValue = param === "position-size" ? "position-size" : "profit";
+
+  const updateQuery = (value: string) => {
+    const newQuery = new URLSearchParams(window.location.search);
+    newQuery.set("type", value); // Update the query parameter
+    const url = `${pathname}?${newQuery.toString()}`; // Use asPath to construct the new URL
+    router.push(url);
+  };
 
   return (
     <Tabs
       defaultValue={defaultValue}
+      onValueChange={(value) => updateQuery(value)}
       className="w-[360px] sm:w-[400px] md:w-[760px]"
     >
       <TabsList className="grid w-full grid-cols-2">
