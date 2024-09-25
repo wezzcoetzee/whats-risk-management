9020b7f8159dccb4318a4dde768515c4a07f370b
Wesley Coetzee <coetzeewr@gmail.com>
2024-09-25 10:03:32 +0200

    feat: add manifest for pwa wip

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "whatsriskmanagement",
+    short_name: "whatsriskmanagement",
+    description: "helping you manage your risk while trading.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
