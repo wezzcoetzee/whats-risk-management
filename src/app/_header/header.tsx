53a08fbb01647ffd571e3f1145f36f9b7a8570cc
Wesley Coetzee <coetzeewr@gmail.com>
2024-09-25 10:26:18 +0200

    feat: add background to header and footer

--- src/app/_header/header.tsx
+++ src/app/_header/header.tsx
@@ -3,13 +3,13 @@ import { ThemeToggle } from "./component/theme";
 
 export function Header() {
   return (
-    <div className="fixed top-0 left-0 w-full z-10 p-4 ">
+    <header className="fixed top-0 left-0 w-full z-10 p-4 ">
       <div className="w-100 flex justify-between">
         <Link href="/">
           <h1 className="text-2xl font-bold">whatsriskmanagement.</h1>
         </Link>
         <ThemeToggle />
       </div>
-    </div>
+    </header>
   );
 }
