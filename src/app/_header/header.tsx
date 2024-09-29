f0fcd444330d8867dca6ff85aaddc8286d9de2e8
Wesley Coetzee <coetzeewr@gmail.com>
2024-09-29 16:33:40 +0200

    feat: add colors

--- src/app/_header/header.tsx
+++ src/app/_header/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="fixed top-0 left-0 w-full z-10 p-4 ">
       <div className="w-100 flex justify-between">
         <Link href="/">
-          <h1 className="text-2xl font-bold">whatsriskmanagement.</h1>
+          <h1 className="text-2xl font-bold text-primary">whatsriskmanagement.</h1>
         </Link>
         <ThemeToggle />
       </div>
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#000000",
+    theme_color: "#f7931a",
     icons: [
       {
         src: "/icon-192x192.png",
