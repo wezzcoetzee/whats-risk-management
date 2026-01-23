18fdc27102265d3027d93dba22d83afe25562377
Wesley <coetzeewr@gmail.com>
2026-01-24 08:18:20 +1300

    chore: add tests to merges

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
       provider: 'v8',
       include: ['src/lib/**/*.ts'],
       exclude: ['**/*.test.ts'],
+      reporter: ['text', 'text-summary', 'json-summary', 'html'],
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
     },
   },
   resolve: {
